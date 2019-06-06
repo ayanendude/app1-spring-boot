0f27b164dccca0edbedfa843b2c63403ad186e18
ayanendude <ayanendu_de@yahoo.co.uk>
2019-06-06 18:15:22 -0400

    first

--- src/main/java/com/personal/ayanendu/App1springboot/TestHello/TestHello.java
+++ src/main/java/com/personal/ayanendu/App1springboot/TestHello/TestHello.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestHello {
     @GetMapping
     public String hello (){
-        return "Hello World 12";
+        return "Hello World 15";
     }
 }
